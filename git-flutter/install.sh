aad902030e5e68d83b33405fa3b38d5ae1c030fb
Emanuel Fernandes <97025555+efernandes-ppb@users.noreply.github.com>
2022-03-25 10:34:36 +0000

    feat: Improve `install.sh` (#93)
    * check for .zshenv profile file
    
    * remove quotes
    
    * create a git-flutter bin folder

--- git-flutter/install.sh
+++ git-flutter/install.sh
@@ -62,13 +62,15 @@ DETECTED_PROFILE=''
 
 if [ "${SHELL#*bash}" != "$SHELL" ]; then
     if [ -f "$HOME/.bashrc" ]; then
-    DETECTED_PROFILE="$HOME/.bashrc"
+        DETECTED_PROFILE="$HOME/.bashrc"
     elif [ -f "$HOME/.bash_profile" ]; then
-    DETECTED_PROFILE="$HOME/.bash_profile"
+        DETECTED_PROFILE="$HOME/.bash_profile"
     fi
 elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
     if [ -f "$HOME/.zshrc" ]; then
-    DETECTED_PROFILE="$HOME/.zshrc"
+        DETECTED_PROFILE="$HOME/.zshrc"
+    elif [ -f "$HOME/.zshenv" ]; then
+        DETECTED_PROFILE="$HOME/.zshenv"
     fi
 fi
 
@@ -124,15 +126,19 @@ gf_install() {
         gf_echo >&2 "Failed to download '$URL'"
         return 1
     }
-    gf_echo "=> git-flutter latest version: '$LATEST_VERSION'"
+    gf_echo "=> git-flutter latest version: $LATEST_VERSION"
 
     local INSTALL_DIR
     INSTALL_DIR="$(gf_default_install_dir)"
+    local INSTALL_DIR_PKG
+    INSTALL_DIR_PKG="$INSTALL_DIR/pkg"
+    local INSTALL_DIR_BIN
+    INSTALL_DIR_BIN="$INSTALL_DIR/bin"
     local TEMP_ARCHIVE_FILE
     TEMP_ARCHIVE_FILE="$(mktemp -d)/git-flutter${ARCHIVE_SUFFIX}"
 
-    mkdir -p "$INSTALL_DIR"
-    if [ -f "$INSTALL_DIR/git-flutter" ]; then
+    mkdir -p "$INSTALL_DIR_PKG"
+    if [ -f "$INSTALL_DIR_PKG/git-flutter" ]; then
         gf_echo "=> git-flutter is already installed in $INSTALL_DIR, trying to update"
     else
         gf_echo "=> Downloading git-flutter archive '$TEMP_ARCHIVE_FILE'"
@@ -150,32 +156,36 @@ gf_install() {
 
     # Extracttion and file copy
     case "$TEMP_ARCHIVE_FILE" in
-        *.tar.gz) tar xzf "$TEMP_ARCHIVE_FILE" -C "$INSTALL_DIR" ;;
-        *.zip) unzip -o -qq "$TEMP_ARCHIVE_FILE" -d "$INSTALL_DIR" ;;
+        *.tar.gz) tar xzf "$TEMP_ARCHIVE_FILE" -C "$INSTALL_DIR_PKG" ;;
+        *.zip) unzip -o -qq "$TEMP_ARCHIVE_FILE" -d "$INSTALL_DIR_PKG" ;;
     esac
 
-    chmod a+x "$INSTALL_DIR/git-flutter" || {
-        gf_echo >&2 "Failed to mark '$INSTALL_DIR/git-flutter' as executable"
+    chmod a+x "$INSTALL_DIR_PKG/git-flutter" || {
+        gf_echo >&2 "Failed to mark '$INSTALL_DIR_PKG/git-flutter' as executable"
         return 3
     }
 
+    # Create bin symbolic link
+    mkdir -p "$INSTALL_DIR_BIN"
+    ln -sf "$INSTALL_DIR_PKG/git-flutter" "$INSTALL_DIR_BIN/git-flutter"
+
     local GF_PROFILE
     GF_PROFILE="$(gf_detect_profile)"
-    SOURCE_STR="\\n[ -d $INSTALL_DIR ] && export PATH=\"$INSTALL_DIR:\$PATH\"\\n"
+    SOURCE_STR="\\n[ -d $INSTALL_DIR_BIN ] && export PATH=\"$INSTALL_DIR_BIN:\$PATH\"\\n"
 
     if [ -z "${GF_PROFILE-}" ] ; then
         local TRIED_PROFILE
         if [ -n "${PROFILE}" ]; then
         TRIED_PROFILE="${GF_PROFILE} (as defined in \$PROFILE), "
         fi
-        gf_echo "=> Profile not found. Tried ${TRIED_PROFILE-}~/.bashrc, ~/.bash_profile, ~/.zshrc, and ~/.profile."
+        gf_echo "=> Profile not found. Tried ${TRIED_PROFILE-}~/.bashrc, ~/.bash_profile, ~/.zshrc, ~/.zshenv, and ~/.profile."
         gf_echo "=> Create one of them and run this script again"
         gf_echo "   OR"
         gf_echo "=> Append the following lines to the correct file yourself:"
         command printf "${SOURCE_STR}"
         gf_echo
     else
-        if ! command grep -qc "$INSTALL_DIR" "$GF_PROFILE"; then
+        if ! command grep -qc "$INSTALL_DIR_BIN" "$GF_PROFILE"; then
             gf_echo "=> Appending git-flutter source to $GF_PROFILE"
             command printf "${SOURCE_STR}" >> "$GF_PROFILE"
         else
@@ -185,7 +195,7 @@ gf_install() {
 
     gf_echo "=> 🎉 git-flutter was installed successfully 🎉"
 
-    if ! command echo "$PATH" | grep -qc "$INSTALL_DIR"; then
+    if ! command echo "$PATH" | grep -qc "$INSTALL_DIR_BIN"; then
         gf_echo "=> Close and reopen your terminal to start using git-flutter or run the following to use it now:"
         command printf "${SOURCE_STR}"
     fi
